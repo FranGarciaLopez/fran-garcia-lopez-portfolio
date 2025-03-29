4500e47266f11ed1aa6b354bd4d48481ecd3d691
Fran <frangalo34@gmail.com>
2025-03-29 14:27:30 +0100

    add new projects to the sitemaps

--- src/pages/sitemap-es.xml.ts
+++ src/pages/sitemap-es.xml.ts
@@ -1,39 +1,42 @@
-import { APIContext } from 'astro';
+import { APIContext } from "astro";
 
-const baseUrl = 'https://fran-garcia-lopez.com';
+const baseUrl = "https://fran-garcia-lopez.com";
 
 const projects = [
-  { id: 1, lastmod: '2025-03-08' },
-  { id: 2, lastmod: '2025-03-07' },
-  { id: 3, lastmod: '2025-03-06' }
+  { id: 1, lastmod: "2025-03-08" },
+  { id: 2, lastmod: "2025-03-07" },
+  { id: 3, lastmod: "2025-03-06" },
+  { id: 4, lastmod: "2025-03-06" },
+  { id: 5, lastmod: "2025-03-06" },
+  { id: 6, lastmod: "2025-03-06" },
 ];
 
 export async function GET({ request }: APIContext) {
   const urls = [
     {
       loc: `${baseUrl}/es`,
-      lastmod: '2025-03-08',
-      changefreq: 'daily',
-      priority: 1.0
+      lastmod: "2025-03-08",
+      changefreq: "daily",
+      priority: 1.0,
     },
     {
       loc: `${baseUrl}/es/work`,
-      lastmod: '2025-03-08',
-      changefreq: 'monthly',
-      priority: 0.8
+      lastmod: "2025-03-08",
+      changefreq: "monthly",
+      priority: 0.8,
     },
     {
       loc: `${baseUrl}/es/about`,
-      lastmod: '2025-03-08',
-      changefreq: 'weekly',
-      priority: 0.9
+      lastmod: "2025-03-08",
+      changefreq: "weekly",
+      priority: 0.9,
     },
     ...projects.map((project) => ({
       loc: `${baseUrl}/es/projects/${project.id}/`,
       lastmod: project.lastmod,
-      changefreq: 'weekly',
-      priority: 0.8
-    }))
+      changefreq: "weekly",
+      priority: 0.8,
+    })),
   ];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
@@ -49,13 +52,13 @@ ${urls
   </url>
 `
   )
-  .join('')}
+  .join("")}
 </urlset>`;
 
   return new Response(sitemap, {
     headers: {
-      'Content-Type': 'application/xml; charset=utf-8',
-      'Cache-Control': 'max-age=0, s-maxage=3600'
-    }
+      "Content-Type": "application/xml; charset=utf-8",
+      "Cache-Control": "max-age=0, s-maxage=3600",
+    },
   });
 }
